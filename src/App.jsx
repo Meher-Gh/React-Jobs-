2939c5511ec753cacbdd47a26fe0b29cb5a98fed
gharsallah meher <gharsallahmeher1@gmail.com>
2024-03-28 01:51:49 +0100

    hero and home-cards components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import Hero from './components/Hero'
+import HomeCards from './components/HomeCards'
 
 const App = () => {
   return (
-    <NavBar />
+    <>
+      <NavBar />
+      <Hero />
+      <HomeCards />
+    </>
+
   )
 }
 
